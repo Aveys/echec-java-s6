ea5415f048202d52546891752d17ef8611f9e8c3
veysarthur@gmail.com <veysarthur@gmail.com@efac07b0-425f-4dbc-9e45-164d34530f88>
2014-06-16 12:19:33 +0000

    Supression classes inutile

---
+++ trunk/src/com/echec/core/Piece.java
@@ -0,0 +1,75 @@
+package com.echec.core;
+
+
+/**
+ * Created by arthurveys on 16/06/2014.
+ */
+public abstract class Piece {
+	
+	private int x;
+	private int y;
+	private Couleur color;
+	private TypePiece type;
+	
+	
+	//constructeur
+	public Piece(int x, int y, TypePiece type) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Couleur getColor() {
+		return color;
+	}
+
+	public void setColor(Couleur color) {
+		this.color = color;
+	}
+
+	public TypePiece getType() {
+		return type;
+	}
+
+	public void setType(TypePiece type) {
+		this.type = type;
+	}
+
+
+	//teste la validité de 2 coordonnées
+	public static boolean CoordIsValid(int x, int y){
+		
+		if(x>8 || x<0){
+			
+			return false;
+		}
+		if(y>8 || y<0){
+			
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	//methode abstraite vérifiant si le déplacement est possible
+	public abstract boolean DepIsValid();
+}
--- trunk/src/com/echec/core/color.java
+++
@@ -1,7 +0,0 @@
-
-public enum color {
-	
-	blanc,
-	noir;
-
-}
