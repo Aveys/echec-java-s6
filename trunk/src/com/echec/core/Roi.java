ed94f2ef44dc21b25f25f46d4e601b0e79675b93
msb.rico@gmail.com <msb.rico@gmail.com@efac07b0-425f-4dbc-9e45-164d34530f88>
2014-06-16 12:51:34 +0000

    classe Roi terminée

--- trunk/src/com/echec/core/Roi.java
+++ trunk/src/com/echec/core/Roi.java
@@ -8,17 +8,27 @@ public class Roi extends Piece{
 		// TODO Auto-generated constructor stub
 	}
 
+	
 	@Override
 	public boolean DepIsValid(int x, int y) {
 		// TODO Auto-generated method stub
 		
-		
+		//vérification des coordonnées cibles
 		if(CoordIsValid(x, y)){
 			
+			//récupération des coordonnées de la pièce
+			int x_dep = this.getX();
+			int y_dep = this.getY();
 			
+			//si les coordonnées cibles sont à 1 case d'écart avec les coordonnées de départ
+			if( (x >= (x_dep-1)) && (x <= (x_dep+1)) ){
+				
+				if((y >= (y_dep-1)) && (y <= (y_dep+1))){
+					
+					return true;
+				}
+			}
 		}
-		
-		
 		return false;
 	}
 
